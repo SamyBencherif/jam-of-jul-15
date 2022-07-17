14731aeed0e55ebe91a54a429f56c10ece0e256a
Samy Bencherif <samybencherif@live.com>
2022-07-17 01:37:47 -0400

    working on lighting, bug

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 linux:
-	gcc -o program -Iraylib/src source.c -lraylib -flto -lm -ldl -lpthread -lX11 -lxcb -lGL -lGLX -lXext -lGLdispatch -lXau -lXdmcp
+	gcc -o play -Iraylib/src source.c -lraylib -flto -lm -ldl -lpthread -lX11 -lxcb -lGL -lGLX -lXext -lGLdispatch -lXau -lXdmcp
--- source.c
+++ source.c
@@ -1,6 +1,39 @@
 #include "raylib.h"
 #include <math.h>
 
+Color light(Color src, float index)
+{
+  // index=+1 : returns src slightly lighter
+  // index= 0 : returns src
+  // index=-1 : returns src slightly darker
+
+  Color blackpoint = {23, 20, 31, 255};
+  Color whitepoint = {214, 242, 237, 255};
+
+  Vector3 blk_hsv = ColorToHSV(blackpoint);
+  Vector3 wht_hsv = ColorToHSV(whitepoint);
+
+  Vector3 src_hsv = ColorToHSV(src);
+
+  float gradations = .1;
+  int compFunction(int a, int b, int w)
+  {
+    if (index == 0)
+      return a;
+    else if (index > 0)
+      return a+(w-a)*gradations*index;
+    else
+      return a+(b-a)*gradations*(-index);
+  }
+
+  Vector3 comp_hsv;
+  comp_hsv.x = compFunction(src_hsv.x, blk_hsv.x, wht_hsv.x);
+  comp_hsv.y = compFunction(src_hsv.y, blk_hsv.y, wht_hsv.y);
+  comp_hsv.z = compFunction(src_hsv.z, blk_hsv.z, wht_hsv.z);
+
+  return ColorFromHSV(comp_hsv.x, comp_hsv.y, comp_hsv.z);
+}
+
 int main()
 {
     int screenWidth = 800;
@@ -9,7 +42,7 @@ int main()
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(screenWidth, screenHeight, "raylib");
     
-    SetTargetFPS(60);
+    SetTargetFPS(10);
     SetExitKey(0);
 
     // Initialize the camera
@@ -37,8 +70,19 @@ int main()
 
       // https://www.raylib.com/examples.html "Waving cubes"
       DrawGrid(10, 5.0f);
-      int size = 4;
-      DrawCube((Vector3){0,2,0}, size,size,size, BLACK);
+      Color beige = (Color){190, 162, 113, 255};
+      Vector3 s = (Vector3){1,3,10};
+
+      // this should draw three shades of beige cubes
+      // but it calls to `light` are currenlty just returning black
+      Vector3 pos = (Vector3){0,2,0};
+      DrawCube(pos, 4*s.x, 4*s.y, 4*s.z, light(beige, -1));
+
+      pos = (Vector3){4,2,0};
+      DrawCube(pos, 4*s.x, 4*s.y, 4*s.z, beige);
+
+      pos = (Vector3){8,2,0};
+      DrawCube(pos, 4*s.x, 4*s.y, 4*s.z, light(beige, 1));
 
       EndMode3D();
       DrawFPS(10, 10);
